b641c014ada01dabe325886df1bc14cfb137e436
STEVE CAYA <STEVECAYA@gmail.com>
2015-11-19 13:26:15 -0500

    login 0.1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Auth\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -12,5 +12,15 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/auth/login');
 });
+
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
