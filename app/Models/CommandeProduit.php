e868bf3e26a5df2655bb10707d31f37c0f9c5fa9
GBEY0402 <rickdbrochu05@hotmail.fr>
2015-12-03 09:53:53 -0500

    ajout des objets Recette et CommandeProduit

---
+++ app/Models/CommandeProduit.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CommandeProduit extends Model
+{
+
+	/*
+     *L'objet CommandeProduit utilise la table CommandeProduits
+     */
+
+    protected $table = 'CommandesProduits'
+}
---
+++ app/Models/Recette.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recette extends Model
+{
+    /*
+     *L'objet Recette utilise la table Recettes
+     */
+
+    protected $table = 'Recettes';
+}
