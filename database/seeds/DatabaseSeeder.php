b5595d61b94785184dcc29563ba3e161aa584ef0
GBEY0402 <rickdbrochu05@hotmail.fr>
2016-03-11 13:07:52 -0500

    Database blank

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,15 +16,8 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call('UserTableSeeder');
-        $this->call('ClientsTableSeeder');
-        $this->call('CommandesTableSeeder');
-        $this->call('ProduitsFinisTableSeeder');
-        $this->call('MatieresPremieresTableSeeder');
         $this->call('EntrepotTableSeeder');
-        /*$this->call('RecettesTableSeeder');
-        $this->call('CommandesProduitsTableSeeder');*/
         
-
         Model::reguard();
     }
 }
@@ -37,28 +30,11 @@ class UserTableSeeder extends Seeder
         DB::table('users')->delete();
         DB::table('password_resets')->delete();
         $user = new User();
-        $user->prenom = 'Bob';
+        $user->prenom = 'Robert';
         $user->nom = 'Durocher';
         $user->username = 'admin';
         $user->password = Hash::make('admin');
         $user->role = 'Administrateur';
         $user->save();
-
-        $user2 = new User();
-        $user2->prenom = 'Steve';
-        $user2->nom = 'Caya';
-        $user2->username = 'steve';
-        $user2->password = Hash::make('steve');
-        $user2->role = 'dirProd';
-        $user2->save();
-
-        $user3 = new User();
-        $user3->prenom = 'Eric';
-        $user3->nom = 'Brochu';
-        $user3->username = 'eric';
-        $user3->password = Hash::make('eric');
-        $user3->role = 'employe';
-        $user3->save();
-
     }
 }
